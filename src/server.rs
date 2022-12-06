c1d535de6245505e8c59b6d40cd1d5723d96b447
Haruka <mrx@hcc.im>
2022-12-07 00:02:46 +0900

    fix again

--- src/server.rs
+++ src/server.rs
@@ -667,7 +667,7 @@ impl Server {
                     if let Err(e) = accounting_sender
                         .send(AccountingMessage::NewShare(
                             prover_state.read().await.address().to_string(),
-                            proof_difficulty.min(prover_target * 2),
+                            proof_difficulty.min(global_proof_target * 2),
                         ))
                         .await
                     {
